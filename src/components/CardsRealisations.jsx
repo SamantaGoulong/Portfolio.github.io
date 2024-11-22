618d1d36eda997675199389703e0eee6d3df602f
Samanta Goulong <samanta1004@hotmail.com>
2024-11-22 19:11:14 +0100

    feat correction json

--- src/components/CardsRealisations.jsx
+++ src/components/CardsRealisations.jsx
@@ -17,33 +17,18 @@ function CardsRealisations() {
             {data.slice(0, visibleProjects).map((item) => (
                 <div className="realisations-card-content" key={item.id}>
                     <div className="card-images  zoom-in-out-box">
-                        <a
-                            href={item.linkGithub}
-                            className="card-links-button-text"
-                        >
-                            <img
-                                src={item.picture}
-                                alt={item.title}
-                                className="img-card"
-                            />
-
+                        <a href={item.linkGithub} className="card-links-button-text">
+                            <img src={item.picture} alt={item.title} className="img-card"/>
                             <div className="overlay">
                                 <p className="title-project">{item.title}</p>
-                                <p className="description-project">
-                                    {item.description}
-                                </p>
+                                <p className="description-project">{item.description}</p>
                             </div>
                         </a>
                     </div>
                     <div className="card-links">
-                        <div className="card-links-tags">
-                            {item.tags.map((tag, index) => (
+                        <div className="card-links-tags">{item.tags.map((tag, index) => (
                                 <span key={index}>
-                                    <i
-                                        className={`fab ${
-                                            Object.values(tag)[0]
-                                        } card-links-tags-icone`}
-                                    ></i>
+                                    <i className={`fab ${Object.values(tag)[0]} card-links-tags-icone`}></i>
                                 </span>
                             ))}
                         </div>
@@ -53,10 +38,7 @@ function CardsRealisations() {
 
             {visibleProjects < data.length && (
                 <div className="container-button">
-                    <button
-                        onClick={loadMoreProjects}
-                        className="load-more-button"
-                    >
+                    <button onClick={loadMoreProjects}className="load-more-button">
                         Voir plus
                     </button>
                 </div>
